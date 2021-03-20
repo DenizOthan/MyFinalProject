2598a6637711e76aaa9025c1ef32f072fd42c486
deniz othan <deryadeniz453@gmail.com>
2021-03-21 00:05:27 +0300

    21Mart

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -41,30 +41,30 @@ namespace Business.Concrete
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
-            return new SuccesDataDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
 
             
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return new  SuccessDateResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==id));
+            return new  SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==id));
             
         }
 
         public IDataResult<Product> GetById(int productId)
         {
-            return new SuccessDateResult<List<Product>(_productDal.Get(p=>p.ProductId == productId));
+            return new SuccessDataResult<Product>(_productDal.Get(p=>p.ProductId == productId));
         }
 
         public  IDataResult<List<Product>>  GetByUnitPrice(decimal min, decimal max)
         {
-            return new SuccessDateResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            return new SuccessDateResult<List<Product>>(_productDal.GetProductDetails());
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
 
         }
     }
